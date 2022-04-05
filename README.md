78f6cb82edc954c7aaca06b66889ffdc57444582
Mizok <karasunohina@gmail.com>
2022-04-06 03:04:08 +0800

    toggle menu close when link clicked

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 title: webpack-playground-template
 date: 
 author: Mizok
-version: 0.2.2
+version: 0.2.3
 tags: 
 ---
 
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,56 +1,60 @@
-function initToggler(){
+function initToggler() {
   const ele = document.querySelector('.playground__toggler#playground-toggler');
   const pl = document.querySelector('.playground#playground');
-  ele.addEventListener('click',()=>{
+  ele.addEventListener('click', () => {
     ele.classList.toggle('is-active')
     pl.classList.toggle('playground--active');
   })
 }
 
-function initMenuScroll(){
+function initMenuScroll() {
   const ele = document.querySelector('.menu#menu');
   const inner = document.querySelectorAll('.menu#menu .menu__inner');
-  const handler = (eventTarget:EventTarget|HTMLElement)=>{
+  const handler = (eventTarget: EventTarget | HTMLElement) => {
     const target = (eventTarget as HTMLElement)
     const scrollTop = target.scrollTop;
-    if(scrollTop>0){
+    if (scrollTop > 0) {
       ele.classList.add('menu--top-shaded')
     }
-    else{
+    else {
       ele.classList.remove('menu--top-shaded')
     }
 
-    if(target.scrollHeight - target.getBoundingClientRect().height - scrollTop > 0){
+    if (target.scrollHeight - target.getBoundingClientRect().height - scrollTop > 0) {
       ele.classList.add('menu--bot-shaded')
     }
-    else{
+    else {
       ele.classList.remove('menu--bot-shaded')
     }
   }
-  
-  inner.forEach((el)=>{
+
+  inner.forEach((el) => {
     handler(el);
-    el.addEventListener('scroll',(e)=>{
+    el.addEventListener('scroll', (e) => {
       handler(e.currentTarget);
     })
-    window.addEventListener('resize',(e)=>{
+    window.addEventListener('resize', (e) => {
       handler(el);
     })
   })
 }
- 
-function initMenuLink(){
+
+function initMenuLink() {
   const ele = document.querySelectorAll('.menu#menu .menu__link');
-  const content = document.querySelector('iframe#playground-content')
-  ele.forEach((el)=>{
-    el.addEventListener('click',()=>{
+  const content = document.querySelector('iframe#playground-content');
+  const toggler = document.querySelector('.playground__toggler#playground-toggler');
+  const pl = document.querySelector('.playground#playground');
+  ele.forEach((el) => {
+    el.addEventListener('click', () => {
+      toggler.classList.remove('is-active');
+      pl.classList.remove('playground--active');
       const route = el.getAttribute('data-route');
-      content.setAttribute('src','examples/'+route+'.html');
+      content.setAttribute('src', 'examples/' + route + '.html');
     })
   })
 }
 
-function main(){
+function main() {
   initToggler();
   initMenuLink();
   initMenuScroll();
