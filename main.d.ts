89db383c212f094c9ef6582560b500dae6b1e851
Mizok <karasunohina@gmail.com>
2022-04-17 21:05:25 +0800

    Updates

--- main.d.ts
+++ main.d.ts
@@ -2,4 +2,6 @@ declare function initToggler(): void;
 declare function initMenuScroll(): void;
 declare function initMenuLink(): void;
 declare function initIframeOnLoad(): void;
+declare function hashHandler(): void;
+declare function initHashMechanic(): void;
 declare function main(): void;
