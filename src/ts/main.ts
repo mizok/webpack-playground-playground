721811029de744046174245075cf9bb881db7ae6
Mizok <karasunohina@gmail.com>
2022-04-17 20:43:20 +0800

    實作hash機制

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -41,17 +41,15 @@ function initMenuScroll() {
 
 function initMenuLink() {
   const ele = document.querySelectorAll('.menu#menu .menu__link');
-  const content = document.querySelector('iframe#playground-content');
-  const loading = document.querySelector('#loading');
   const toggler = document.querySelector('.playground__toggler#playground-toggler');
   const pl = document.querySelector('.playground#playground');
   ele.forEach((el) => {
-    el.addEventListener('click', () => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault();
       toggler.classList.remove('is-active');
       pl.classList.remove('playground--active');
-      loading.classList.add('playground__loading--active')
       const route = el.getAttribute('data-route');
-      content.setAttribute('src', 'examples/' + route + '.html');
+      window.location.hash = `#${route}`;
     })
   })
 }
@@ -64,11 +62,41 @@ function initIframeOnLoad() {
   }, true)
 }
 
+function hashHandler() {
+  const hash = location.hash;
+  const content = document.querySelector('iframe#playground-content');
+  const hashID = hash.replace(/^#(.*)/g, `$1`);
+  const loading = document.querySelector('#loading');
+  const menuLinkElements = document.querySelectorAll('#menu .menu__item .menu__link');
+  const menuLinks = Array.prototype.map.call(menuLinkElements, (element: HTMLElement) => {
+    return element.innerHTML;
+  })
+  if (menuLinks.includes(hashID)) {
+    content.setAttribute('src', 'examples/' + hashID + '.html');
+    loading.classList.add('playground__loading--active')
+  }
+  else {
+    const url = new URL(window.location.href);
+    url.hash = '';
+    history.replaceState(null, document.title, url);
+  }
+}
+
+function initHashMechanic() {
+  hashHandler();
+  window.addEventListener('hashchange', () => {
+    console.log(window.location.hash);
+    hashHandler();
+  })
+}
+
+
 function main() {
   initToggler();
   initMenuLink();
   initMenuScroll();
   initIframeOnLoad();
+  initHashMechanic();
 }
 
 window.onload = main;
